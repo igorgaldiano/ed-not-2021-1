ee28365afccb38d642558217eb1cf5261c83838b
igorgaldiano <igorgaldiano80@gmail.com>
2021-04-06 01:22:38 +0000

    (05/04) Introducao a Stack

---
+++ 16-pilha2.mjs
@@ -0,0 +1,24 @@
+import {Stack} from './lib/Stack.mjs'
+
+const texto = 'Socorram-me, subi no ônibus em Marrocos'
+
+const pilha = new Stack()
+
+// Empilhamento
+
+for(let i=0; i<texto.length; i++){
+    pilha.push(texto.charAt(i))
+
+}
+
+
+console.log(pilha.print()) //observar os elementos da pilha
+
+let textoRev = ''
+
+//Desempilhamento
+while(!pilha.empty){  
+
+    textoRev += pilha.pop()
+}
+console.log(textoRev)
